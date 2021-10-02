41fe41520971c466c3fa274bcd7f719c8b4282ec
Steniotf <steniotfreire@gmail.com>
2021-10-02 13:36:21 -0300

    initial comment

---
+++ README.md
@@ -0,0 +1,17 @@
+# Semáforo
+
+## Vanilla JS
+
+### Javascript
+- Projeto em javascript puro
+- Tecnologias utilizadas Html, Css e Javascript puro
+- Neste projeto o conteúdo aprendido, foi a manipulação de DOM.
+-  A animação executada após a interação do usuário ao clicar nos botôes.
+- Conteúdo desenvolvido a partir de vídeo aula Fernando Leonid
+[Javascript](https://www.youtube.com/watch?v=EujFSEsZsk4&ab_channel=FernandoLeonid)
+
+<br>
+<br>
+<p align="center">
+	<img src="semaforo.gif" alt="Semáforo">
+</p>
---
+++ semaforo.js
@@ -0,0 +1,36 @@
+// Semáforo
+const img = document.getElementById('img');
+const buttons = document.getElementById('buttons');
+let colorIndex =0;
+let intervalId =null;
+
+const trafficLight =(event) =>{
+  stopAutomatic();
+  turnOn[event.target.id]();
+
+  
+}
+
+const nextIndex = () =>  colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+
+
+const changeColors = () =>{
+  const colors =['red', 'yellow', 'green'];
+  const color = colors[colorIndex];
+  turnOn[color]();
+  nextIndex();
+}
+
+const stopAutomatic =() =>{
+    clearInterval( intervalId );
+}
+
+const turnOn ={
+  'red':       () => img.src = './img/vermelho.png',
+  'yellow':    () => img.src = './img/amarelo.png',
+  'green':     () => img.src = './img/verde.png',
+  'automatic': () => intervalId = setInterval( changeColors, 1000 )
+}
+buttons.addEventListener('click', trafficLight);
+
+ 
